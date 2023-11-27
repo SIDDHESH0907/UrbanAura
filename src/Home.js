464e0f29c28f5010518eb3f2c6a659bb37f102d6
Siddhesh <gaonkarsiddhesh0907@gmail.com>
2023-11-27 20:39:06 +0530

    carosel

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import "./Home.css";
 import { products } from "./Data";
 import HomeProductGrid from "./HomeProductGrid";
 import Navbar from "./Navbar";
+import ProductCarousel from "./ProductCarousel"; // Adjust the path as needed
 
 export default function Home() {
   return (
@@ -23,7 +24,41 @@ export default function Home() {
         <HomeProductGrid products={products} />
       </div>
 
-      <div className="Poster"></div>
+      <div className="poster-container">
+        <div className="poster-child">
+          <div className="left1">
+            <p className="poster-heading">Creative harmonious living</p>
+            <p className="poster-para">
+              RAOUF Products are all made to standard sizes so that you can mix
+              and match them freely.
+            </p>
+            <button className="poster-btn">SHOP NOW</button>
+          </div>
+        </div>
+        <div className="poster-child">
+          <div className="right1"></div>
+        </div>
+      </div>
+
+      {/* <div className="poster-child"> */}
+        <ProductCarousel products={products} /> {/* Add this line */}
+      {/* </div> */}
+
+      <div className="poster-container">
+        <div className="poster-child">
+          <div className="left2"></div>
+        </div>
+        <div className="poster-child">
+          <div className="right2">
+            <p className="poster-heading">Creative harmonious living</p>
+            <p className="poster-para">
+              RAOUF Products are all made to standard sizes so that you can mix
+              and match them freely.
+            </p>
+            <button className="poster-btn">SHOP NOW</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/ProductCarousel.js
@@ -0,0 +1,65 @@
+// ProductCarousel.js
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './ProductCarousel.css';
+
+const ProductCarousel = ({ products }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const sliderRef = React.useRef();
+
+  const handlePrev = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  const handleNext = () => {
+    sliderRef.current.slickNext();
+  };
+
+  return (
+    <div className="product-carousel-container">
+      <Slider ref={sliderRef} {...settings}>
+        {products.map((product) => (
+          <div key={product.id} className="product-slide">
+            <img src={product.imageUrl} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>₹ {product.price}</p>
+          </div>
+        ))}
+      </Slider>
+      <button className="prev" onClick={handlePrev}>
+        Prev
+      </button>
+      <button className="next" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default ProductCarousel;
